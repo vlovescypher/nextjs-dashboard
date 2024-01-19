09c0e280bb341b3bd44405937abc7d1beb9a4e9d
Tina Klie <tina.klie@exxeta.com>
2024-01-19 09:08:25 +0100

    add search to invoices page

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,21 @@
+import { lusitana } from '@/app/ui/fonts';
+import { CreateInvoice } from '@/app/ui/invoices/buttons';
+import Search from '@/app/ui/search';
+
 export default function Page() {
-  return <p>Invoices Page</p>;
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      {/* Suspense Table */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* Pagination */}
+      </div>
+    </div>
+  );
 }
